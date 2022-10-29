9c677e1aff1a5dfe686f502cf3d560342f067ea5
Mansi Sharma <mansi.isha.mansi@gmail.com>
2022-10-29 11:20:27 +0530

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Auth from './pages/Auth/Auth';
+import Home from './pages/Home/Home';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      sTEAMedSaMoSA
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route exact path="/auth" element={<Auth />} />
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
