5e5783987e1c20d11f7dcc8a7b3334f95f12cbba
Aditi Tiwari <adititiwari@Aditis-MacBook-Pro.local>
2022-11-06 02:53:40 +0530

    removed unsused commented part

--- src/pages/WalletPage/WalletPage.jsx
+++ src/pages/WalletPage/WalletPage.jsx
@@ -49,4 +49,3 @@ function WalletPage() {
 }
 
 export default WalletPage;
-// style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh', fontSize: '40px', fontWeight: '800' }}
