ea9955e1f1b65410ed241bd0f5485a39aa8e2df0
Aditi Tiwari <adititiwari@Aditis-MacBook-Pro.local>
2022-11-06 02:52:43 +0530

    Added the wallets page

--- src/pages/SchedulePage/SchedulePage.jsx
+++ src/pages/SchedulePage/SchedulePage.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
+import { ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, Inject } from '@syncfusion/ej2-react-schedule';
 import './SchedulePage.scss';
-// import ScheduleCalendar from '../../components/ScheduleCalendar/ScheduleCalendar';
-import ScheduleHeader from '../../components/ScheduleHeader/ScheduleHeader';
 
 function SchedulePage() {
+  const data = [
+    {
+      Id: 1,
+      Subject: 'Meeting - 1',
+      StartTime: new Date(2018, 1, 15, 10, 0),
+      EndTime: new Date(2018, 1, 15, 10, 0),
+      IsAllDay: true,
+    }];
   return (
-    <div className="schedulePage" id="dialog-target-schedule">
-      <div className="schedulePage__content">
-        <ScheduleHeader />
-        {/* <ScheduleCalendar /> */}
+    <div className="schedule">
+      <div><p className="heading">Time slots</p></div>
+      <div><p className="text">Select the block of time slot, according to your availability</p></div>
+      <div className="control-pane">
+        <div className="control-section">
+          <div className="calendar-control-section" style={{ overflow: 'auto' }}>
+            <ScheduleComponent
+              selectedDate={new Date(2018, 1, 15)}
+              eventSettings={{ dataSource: data }}
+              currentView="Month"
+            >
+              <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
+            </ScheduleComponent>
+          </div>
+        </div>
       </div>
     </div>
   );
